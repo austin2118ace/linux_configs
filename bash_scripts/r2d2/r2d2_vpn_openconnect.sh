476ed245240ee062a26a2234212c48c06ccacd64
Austin Pauley <austin2118ace@gmail.com>
2025-04-02 00:06:28 -0400

    update script

--- bash_scripts/r2d2/r2d2_vpn_openconnect.sh
+++ bash_scripts/r2d2/r2d2_vpn_openconnect.sh
@@ -5,10 +5,10 @@ server_IP=//128.186.180.118/r2d2
 mount_dir=/mnt/r2d2
 cred_file=/home/austin/creds/.r2d2_creds
 
-if [[ ! -d "$mount_dir" ]]; then
-	echo "Mount directory does not exist! Creating directory..."
-	sudo mkdir $mount_dir
-fi
+# if [[ ! -d "$mount_dir" ]]; then
+# 	echo "Mount directory does not exist! Creating directory..."
+# 	sudo mkdir $mount_dir
+# fi
 
 # if ping -c 1 "$_server_IP" &> /dev/null
 # then
@@ -18,8 +18,9 @@ fi
 echo "Connecting to vpn.fsu.edu!"
 echo "Press any key once connected to vpn..."
 
-eval "$(conda shell.bash hook)"
-conda activate openconnect
+#eval "$(conda shell.bash hook)"
+#conda activate openconnect
+source "/opt/dev/repo/apps/openconnect-sso/vpn/bin/activate"
 openconnect-sso &
 
 read -s -n 1
